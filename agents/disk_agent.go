8a4733dc1d52ec65bca4715f7d435c31545d91bd
Ben Meier <benmeier@fastmail.com>
2017-12-25 21:40:07 +0200

    tweak log message

--- agents/disk_agent.go
+++ agents/disk_agent.go
@@ -60,6 +60,7 @@ func (a *diskAgent) Tick(s sink.Sink) error {
 
 			usage, uerr := disk.Usage(p.Mountpoint)
 			if uerr == nil {
+				log.Printf("Outputting Usage for %v because it matched device_regex", p)
 				prefixPath := fmt.Sprintf("%s.%s", a.config.Path, a.formatDeviceName(p.Device))
 
 				s.Gauge(fmt.Sprintf("%s.total", prefixPath), float64(usage.Total))
@@ -93,7 +94,7 @@ func (a *diskAgent) Tick(s sink.Sink) error {
 				}
 			}
 
-			log.Printf("Outputting metrics for %v because it matched device_regex", deviceName)
+			log.Printf("Outputting IO Counters for %v because it matched device_regex", iostat)
 			prefixPath := fmt.Sprintf("%s.%s", a.config.Path, a.formatDeviceName(deviceName))
 
 			s.Gauge(fmt.Sprintf("%s.read_count", prefixPath), float64(iostat.ReadCount))
